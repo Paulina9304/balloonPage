ead0f73afa4f34de4f7a0cc96849fac0a53da124
Paulina <paulinafijal2@gmail.com>
2020-01-31 18:57:46 +0100

    create animBg

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const elements = document.querySelectorAll('.anim-bg');
+    const animBg = new AnimBg('.anim-bg')
 
-    document.addEventListener('mousedown', function(event) {
-    const { clientX, clientY } = event;
-    const centerX = window.innerWidth / 2;
-    const centerY= window.innerHeight / 2;
-
-    elements.forEach(element => {
-        const ratioX = -element.getAttribute('ratioX');
-        const ratioY = -element.getAttribute('ratioY');
-
-        const moveX= clientX - centerX;
-        const moveY= clientY - centerY;
-
-        element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY}px)`;
-    })
-    })
+    document.addEventListener('mousedown', (event) => animBg.listenCursorMove(event)) 
+    
 })
